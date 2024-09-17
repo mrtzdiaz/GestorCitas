d54338d786a5a8319c4980cf98e0ac4a0a89861a
Daniel <mrtzdiazdaniel@gmail.com>
2024-09-17 11:05:47 -0600

    Spring Boot Setup

---
+++ src/main/java/com/hospital/GestorCitas/GestorCitasApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.GestorCitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/GestorCitas/GestorCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.GestorCitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
